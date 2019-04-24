4b1939634d4674031350ea220768d5b4d081a0d5
Krzysztof Rogoziński <kris@makolab.com>
2019-04-24 15:28:15 +0200

    Auth with NgRx

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,19 +1,28 @@
 import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
-import {AuthService} from '../auth/auth.service';
+import {Store} from '@ngrx/store';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/switchMap';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) {
+  constructor(private store: Store<fromApp.AppState>) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', req);
-    const copiedReq = req.clone({
-      params: req.params.append('auth', this.auth.getToken())
+
+    return this.store.select('auth')
+      .take(1)
+      .switchMap((authState: fromAuth.State) => {
+      const copiedReq = req.clone({
+        params: req.params.append('auth', authState.token)
+      });
+      return next.handle(copiedReq);
     });
-    return next.handle(copiedReq);
   }
 }
